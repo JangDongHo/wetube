50e5457e900515d86df4863c8da1a184e847553e
Jang-DongHo <donghoo18@naver.com>
2022-03-08 16:00:38 +0900

    유튜브 클론코딩 #3.0 ~ #3.4 추가

---
+++ src/server.js
@@ -0,0 +1,21 @@
+import express from "express";
+
+const PORT = 4000;
+
+const app = express();
+
+const handleHome = (req, res) => {
+  return res.end();
+};
+
+const handleLogin = (req, res) => {
+  return res.send({ message : "Login here." });
+};
+
+app.get("/", handleHome);
+app.get("/login", handleLogin);
+
+const handleListening = () => 
+  console.log(`Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);
