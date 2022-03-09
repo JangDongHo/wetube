1e61455a3933f87878d1a0e297392d47fd9aa2f8
Jang-DongHo <donghoo18@naver.com>
2022-03-09 16:12:21 +0900

    유튜브 클론코딩 #3.5 ~ #3.10 내용 추가

--- src/server.js
+++ src/server.js
@@ -1,21 +1,47 @@
 import express from "express";
 
-const PORT = 4000;
-
 const app = express();
+
+const urlLogger = (req, res, next) => {
+  console.log(`Path: ${req.url}`);
+  next();
+};
+
+const timeLogger = (req, res, next) => {
+  const now = new Date();
+  console.log(
+    `Time: ${now.getFullYear()}.${now.getMonth() + 1}.${now.getDate()}`
+  );
+  next();
+};
+
+const securityLogger = (req, res, next) => {
+  const protocol = req.protocol;
+  if (protocol === "https") {
+    console.log("Secure");
+  } else {
+    console.log("Insecure");
+  }
+  next();
+};
+
+const protectorMiddleware = (req, res, next) => {
+  const url = req.url;
+  if (url === "/protected") {
+    res.send("Do not allowed");
+  }
+  next();
+};
 
 const handleHome = (req, res) => {
   return res.end();
 };
 
-const handleLogin = (req, res) => {
-  return res.send({ message : "Login here." });
+app.use(urlLogger, timeLogger, securityLogger, protectorMiddleware);
+app.get("/", handleHome);
+
+const handleListening = () => {
+  console.log("Server Start!");
 };
 
-app.get("/", handleHome);
-app.get("/login", handleLogin);
-
-const handleListening = () => 
-  console.log(`Server listening on port http://localhost:${PORT}`);
-
-app.listen(PORT, handleListening);
+app.listen(handleListening);
