f7206b8f535617f3592a7c60a94f81485849e3a2
Jang-DongHo <donghoo18@naver.com>
2022-03-10 10:01:33 +0900

    유튜브 클론코딩 #3.11 내용 추가

--- src/server.js
+++ src/server.js
@@ -1,19 +1,18 @@
 import express from "express";
+import morgan from "morgan";
 
 const PORT = 4000;
 
 const app = express();
+const loggerMiddleware = morgan("short");
 
-const logger =  (req, res, next) => {
-  console.log(` ${req.method} ${req.url}`);
-  next();
+const home = (req, res) => {
+  console.log("I will respond.");
+  return res.send("hello");
 }
 
-const handleHome = (req, res) => {
-  return res.end();
-};
-
-app.get("/", logger, handleHome);
+app.use(loggerMiddleware);
+app.get("/", home);
 
 const handleListening = () => 
   console.log(`Server listening on port http://localhost:${PORT}`);
