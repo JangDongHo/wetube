a86ec88b04c829ce5b0b4633894a8c5e4b1623e5
Jang-DongHo <donghoo18@naver.com>
2022-03-14 15:58:56 +0900

    유튜브 클론코딩 #5.0 ~ #5.6

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -1,10 +1,6 @@
-export const trending = (req, res) => res.send("Home Page Videos");
-export const see = (req, res) => {
-  return res.send(`Watch Video #${req.params.id}`);
-}
-export const edit = (req, res) => {
-  res.send("Edit");
-}
+export const trending = (req, res) => res.render("home", { pageTitle: "Home" });
+export const see = (req, res) => res.render("watch");
+export const edit = (req, res) => res.send("Edit");
 export const search = (req, res) => res.send("Search");
 export const upload = (req, res) => res.send("Upload");
 export const deleteVideo = (req, res) => { 
