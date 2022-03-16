e507329650ac1a0e900831e9e253b43225f6e405
Jang-DongHo <donghoo18@naver.com>
2022-03-16 21:15:11 +0900

    수강

--- src/server.js
+++ src/server.js
@@ -9,14 +9,14 @@ const PORT = 4000;
 const app = express();
 const logger = morgan("dev");
 app.use(logger);
-
+app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use("/", globalRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
-const handleListening = () => 
+const handleListening = () =>
   console.log(`Server listening on port http://localhost:${PORT}`);
 
 app.listen(PORT, handleListening);
