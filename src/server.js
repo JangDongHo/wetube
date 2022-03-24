5510ce2d0b1f1e191a4b38bb8814ab73dda36158
Jang-DongHo <donghoo18@naver.com>
2022-03-24 18:56:31 +0900

    강의 추가

--- src/server.js
+++ src/server.js
@@ -1,6 +1,6 @@
 import express from "express";
 import morgan from "morgan";
-import globalRouter from "./routers/globalRouter";
+import rootRouter from "./routers/rootRouter";
 import videoRouter from "./routers/videoRouter";
 import userRouter from "./routers/userRouter";
 
@@ -10,7 +10,7 @@ app.use(logger);
 app.use(express.urlencoded({ extended: true }));
 app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
-app.use("/", globalRouter);
+app.use("/", rootRouter);
 app.use("/videos", videoRouter);
 app.use("/users", userRouter);
 
