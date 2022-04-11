ff8f729d0c08ac35900ff5d575cb4b84b303d9c6
Jang-DongHo <donghoo18@naver.com>
2022-04-11 23:03:50 +0900

    강의 내용 추가

--- src/controllers/videoController.js
+++ src/controllers/videoController.js
@@ -12,7 +12,6 @@ export const home = async (req, res) => {
 export const watch = async (req, res) => {
   const { id } = req.params;
   const video = await Video.findById(id).populate("owner").populate("comments");
-  console.log(video);
   if (!video) {
     return res.render("404", { pageTitle: "Video not found" });
   }
@@ -148,5 +147,5 @@ export const createComment = async (req, res) => {
   });
   video.comments.push(comment._id);
   video.save();
-  return res.sendStatus(201);
+  return res.status(201).json({ newCommentId: comment.id });
 };
